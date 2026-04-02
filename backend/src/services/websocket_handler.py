e0ccfa41e91042c0552ec01dfd5f1c0657243281
Ayo-faks <ayoolafakoya@gmail.com>
2026-04-02 08:37:13 +0100

    revert: back to en-US-Ava:DragonHDLatestNeural (Omni voice had no audio)

--- backend/src/services/websocket_handler.py
+++ backend/src/services/websocket_handler.py
@@ -46,7 +46,7 @@ DEFAULT_NOISE_REDUCTION_TYPE = "azure_deep_noise_suppression"
 DEFAULT_ECHO_CANCELLATION_TYPE = "server_echo_cancellation"
 DEFAULT_AVATAR_CHARACTER = "lisa"
 DEFAULT_AVATAR_STYLE = "casual-sitting"
-DEFAULT_VOICE_NAME = "en-GB-Sonia:DragonHDOmniLatestNeural"
+DEFAULT_VOICE_NAME = "en-US-Ava:DragonHDLatestNeural"
 DEFAULT_VOICE_TYPE = "azure-standard"
 
 # Message types
